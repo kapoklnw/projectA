8179c5ad484498daeeff1ec12a51a392aa423eff
unenternow <manlove0001@gmail.com>
2015-10-28 21:16:19 +0700

    เงื่อนไขการตรวจสอบค่าตอบแทน

--- src/com/pj/A.java
+++ src/com/pj/A.java
@@ -19,6 +19,17 @@ public class A {
 		System.out.print("Input your sales : ");
 		sales = in.nextDouble();					
 		
+		if(sales > 50000){
+			Commission = sales*0.03;
+		}else if(sales > 25000){
+			Commission = sales*0.02;
+		}else if(sales > 1){
+			Commission = sales*0.01;
+		}
+		else{
+			Commission = 0;
+		}
+		compensation = salary+Commission;
 		
 	}
 
