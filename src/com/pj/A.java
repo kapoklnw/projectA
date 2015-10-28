ee3c25a4d2db1ea109753a435440f4f6155120c0
sanzaza <badlist99@gmail.com>
2015-10-28 20:37:12 +0700

    ประกาศตัวแปร กำหนด Input

--- src/com/pj/A.java
+++ src/com/pj/A.java
@@ -2,6 +2,24 @@ package com.pj;
 import java.util.*;
 
 public class A {
-	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name;
+		double salary;
+		double sales;
+		double Commission;
+		double compensation;
+		
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input your name : ");
+		name = in.nextLine();						
+		System.out.print("Input your salary : ");
+		salary = in.nextDouble();					
+		System.out.print("Input your sales : ");
+		sales = in.nextDouble();					
+		
+		
+	}
 
 }
