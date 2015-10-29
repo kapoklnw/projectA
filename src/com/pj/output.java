b3e315e320d8eaef5905a4b30b4af9fea964f3b1
kapokpeaa <body_slam_pea@hotmail.com>
2015-10-29 11:10:23 +0700

    แก้ไขเป็น class แยก

---
+++ src/com/pj/output.java
@@ -0,0 +1,10 @@
+package com.pj;
+
+public class output {
+	public void print(String name,double compensation) {
+		
+
+		System.out.println("\nname : "+name);
+		System.out.println("Commpensation : "+compensation);
+	}
+}
